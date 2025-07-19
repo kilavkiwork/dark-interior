f57595154044aa93499efd1eaca190ebef2a263d
kilavkiwork <kilavki@outlook.com>
2025-07-19 21:47:08 +0300

    feat: base markup to all project

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+import { helloModule, privateMessage } from './modules/module.js';
+
+console.log('hello console this is script from "./scripts/main.js"');
+helloModule(privateMessage);
+
+console.log(5 + '5');
